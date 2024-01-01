69c7443abdfcec8b583a27f06f5c2187728f94df
Mamta sharma <your_email@example.com>
2023-12-31 21:44:33 -0800

    changes styling of 1st row of homepage

--- frontend/src/Components/HomeComponents/Firstrow.jsx
+++ frontend/src/Components/HomeComponents/Firstrow.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-import {  Col, Container,Row } from 'react-bootstrap'
+import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import row1image from '../images/row1.jpg';
@@ -8,31 +8,32 @@ import Image from 'react-bootstrap/Image';
 function Firstrow() {
   return (
     <div>
-      <Container className='mt-5'>
+      <Container className='mt-5 align-item-center justify-content-center' style={{ height: '400px' }}>
         <Row>
-            <Col md={6} >
-            <Card  border='light'>
-                <Card.Body  className="d-flex flex-column align-items-center mt-4" >
-        <Card.Title >Your Health is your priority.</Card.Title>
-        <Card.Text >
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="success" >Book Ticket</Button>{' '}
-
-      </Card.Body>
-
-                </Card>
-            </Col>
-            <Col md={6} className="d-flex justify-content-center align-items-center">
-            <Image src={row1image} alt="" rounded style={{ width: '500px', height: '300px', alignContent:'center' }} />
+          <Col md={6}>
+            <Card border='light' style={{ height: '100%' }}>
+              <Card.Body className="d-flex flex-column justify-content-center align-items-center">
+                <Card.Title style={{ fontSize: '1.9rem',textTransform:'uppercase', color:'skyblue',fontWeight: 'bold', marginBottom: '1rem' }}>
+                  Your Health is your priority
+                </Card.Title>
+                <Card.Text style={{ fontSize: '1.2rem', textAlign: 'center', lineHeight: '2rem', color: '#555' }}>
+                  Some quick example text to build on the card title and make up the
+                  bulk of the card's content text to build on the card title and make up the
+                  bulk of the card's content.
+                </Card.Text>
+                <Button variant="success" style={{ fontSize: '1.5rem', padding: '12px 24px', marginTop: '1rem' }}>
+                  Book Ticket
+                </Button>{' '}
+              </Card.Body>
+            </Card>
           </Col>
-
+          <Col md={6} className="d-flex justify-content-center align-items-center">
+            <Image src={row1image} alt="" rounded style={{ width: '500px', height: '300px', alignContent: 'center' }} />
+          </Col>
         </Row>
       </Container>
     </div>
-  )
+  );
 }
 
-export default Firstrow
+export default Firstrow;
